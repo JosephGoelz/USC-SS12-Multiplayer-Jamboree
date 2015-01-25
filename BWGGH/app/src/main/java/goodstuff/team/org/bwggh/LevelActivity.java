483c1eb878323bd310ead797bb817296e6ce7fe9
Justine Foote <jfoote@usc.edu>
2015-01-25 06:12:08 -0800

    adding the android app

---
+++ BWGGH/app/src/main/java/goodstuff/team/org/bwggh/LevelActivity.java
@@ -0,0 +1,15 @@
+package goodstuff.team.org.bwggh;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Justine Foote on 1/25/2015.
+ */
+public class LevelActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level);
+    }
+}
