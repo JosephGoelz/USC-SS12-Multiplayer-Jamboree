c9ce069d4dbac703707d76376909a72104925d20
Justine Foote <jfoote@usc.edu>
2015-01-25 11:51:33 -0800

    android

--- BWGGH/app/src/main/java/goodstuff/team/org/bwggh/LevelActivity.java
+++ BWGGH/app/src/main/java/goodstuff/team/org/bwggh/LevelActivity.java
@@ -1,7 +1,9 @@
 package goodstuff.team.org.bwggh;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Justine Foote on 1/25/2015.
@@ -11,7 +13,20 @@ public class LevelActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level);
-
-
     }
-}
+
+    public void gotoLevelOne(View v) {
+        Intent intent = new Intent(this, LevelOneActivity.class);
+        startActivity(intent);
+    }
+
+    public void gotoLevelTwo(View v) {
+        Intent intent = new Intent(this, LevelTwoActivity.class);
+        startActivity(intent);
+    }
+    public void gotoLevelThree(View v) {
+        Intent intent = new Intent(this, LevelThreeActivity.class);
+        startActivity(intent);
+    }
+
+  }
